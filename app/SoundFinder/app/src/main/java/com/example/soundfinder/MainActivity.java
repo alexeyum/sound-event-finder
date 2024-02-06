61436c28d135a020db87b4c08e58ad89effa29d7
Alexey Umnov <alexeyum@gmail.com>
2024-02-06 22:30:40 +0400

    Preparing interface and assets

--- app/SoundFinder/app/src/main/java/com/example/soundfinder/MainActivity.java
+++ app/SoundFinder/app/src/main/java/com/example/soundfinder/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.soundfinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+
+        findViewById(R.id.buttonOpen).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
