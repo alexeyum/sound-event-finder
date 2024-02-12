f0d3731d87701744e1b667e645252c4b7dfc9039
Alexey Umnov <alexeyum@gmail.com>
2024-02-12 20:26:09 +0400

    Model loading and stub for prediction

--- app/SoundFinder/app/src/main/java/com/example/soundfinder/MainActivity.java
+++ app/SoundFinder/app/src/main/java/com/example/soundfinder/MainActivity.java
@@ -1,46 +1,114 @@
 package com.example.soundfinder;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.developer.filepicker.controller.DialogSelectionListener;
 import com.developer.filepicker.model.DialogConfigs;
 import com.developer.filepicker.model.DialogProperties;
 import com.developer.filepicker.view.FilePickerDialog;
 
+import org.pytorch.IValue;
+import org.pytorch.LiteModuleLoader;
 import org.pytorch.Module;
+import org.pytorch.Tensor;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
+    TextView tvFilePath;
+    TextView tvPredictionResult;
     FilePickerDialog fileDialog;
 
+    Module soundClassifierModule;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = findViewById(R.id.textView);
+        tvFilePath = findViewById(R.id.tvFilePath);
+        tvPredictionResult = findViewById(R.id.tvPredictionResult);
+
+        try {
+            soundClassifierModule = LiteModuleLoader.load(assetFilePath("basic_convnet.pt"));
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to load model", e);
+        }
 
         setUpFileOpener();
+
+        findViewById(R.id.buttonPredict).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Prepare the input tensor (N, 2)
+                long[] shape = new long[]{1, 1, 384000};
+                Tensor inputTensor = generateTensor(shape);
+
+                // Get the output from the model
+                float[] output = soundClassifierModule
+                        .forward(IValue.from(inputTensor))
+                        .toTensor()
+                        .getDataAsFloatArray();
+
+                // TODO: model returns logits, not probabilities at the moment
+                
+                // Get the output as a string
+                String out = "";
+                for (float l : output) {
+                    out += String.valueOf(l);
+                }
+
+                // Show the output
+                tvPredictionResult.setText(out);
+            }
+        });
+    }
+
+    // Given the name of the pytorch model, get the path for that model
+    public String assetFilePath(String assetName) throws IOException {
+        File file = new File(this.getFilesDir(), assetName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream is = this.getAssets().open(assetName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
+
+    // Generate a tensor of random numbers given the size of that tensor.
+    public Tensor generateTensor(long[] Size) {
+        // Create a random array of floats
+        Random rand = new Random();
+        int fullSize = (int)(Size[0]*Size[1]*Size[2]);
+        float[] arr = new float[fullSize];
+        for (int i = 0; i < fullSize; i++) {
+            arr[i] = -10000 + rand.nextFloat() * (20000);
+        }
+
+        // Create the tensor and return it
+        return Tensor.fromBlob(arr, Size);
     }
 
     protected void setUpFileOpener() {
@@ -62,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         fileDialog.setDialogSelectionListener(new DialogSelectionListener() {
             @Override
             public void onSelectedFilePaths(String[] files) {
-                tv.setText(files[0]);
+                tvFilePath.setText(files[0]);
             }
         });
 
