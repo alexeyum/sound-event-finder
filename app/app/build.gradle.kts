e77ca3096f38be24100a904b2cc29500c29277d6
Alexey Umnov <alexeyum@gmail.com>
2024-06-01 17:42:54 +0400

    Implemented API calls, removed unnecessary stuff

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -38,6 +38,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("org.pytorch:pytorch_android_lite:1.13.1")
     implementation("io.github.tutorialsandroid:filepicker:9.2.5")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
